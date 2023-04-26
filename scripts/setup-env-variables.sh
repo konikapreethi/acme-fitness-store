701695c42318c686e711a75fce12ea5ece545160
konikapreethi <71210086+konikapreethi@users.noreply.github.com>
2023-04-26 08:30:37 +0000

    Update setup-env-variables.sh

--- scripts/setup-env-variables.sh
+++ scripts/setup-env-variables.sh
@@ -1,6 +1,6 @@
-export SUBSCRIPTION=CHANGEME                 # replace it with your subscription-id
-export RESOURCE_GROUP=student-rg-CHANGEME          # existing resource group or one that will be created in next steps
-export SPRING_APPS_SERVICE=student-asae-CHANGEME   # A unique name of the service that will be created in the next steps
+export SUBSCRIPTION=2b662324-593f-4cb9-9e69-7c7ca8916694                 # replace it with your subscription-id
+export RESOURCE_GROUP=asc-training-rg-20          # existing resource group or one that will be created in next steps
+export SPRING_APPS_SERVICE=asc-training-asae-ifdu-20   # A unique name of the service that will be created in the next steps
 export REGION=eastus                          # choose a region with Enterprise tier support
 
 export CART_SERVICE_APP="cart-service"
